cae77344639c64c4c7295d1cae97a7f8fec719ef
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-01-09 12:19:17 -0200

    scaling algorithm

--- src/system.c
+++ src/system.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include <sys/errno.h>
+#include <libmath.h>
 #include <libgte.h>
 #include <libgpu.h>
 #include <libetc.h>
 #include <libapi.h>
-#include <sys/errno.h>
 #include "types.h"
 #include "chip8.h"
 #include "log.h"
@@ -13,16 +14,37 @@
 u_long _ramsize   = 0x00200000; // force 2 megabytes of RAM
 u_long _stacksize = 0x00004000; // force 16 kilobytes of stack
 
+
 uint16_t sys_chip8_gfx[CHIP8_HEIGHT][CHIP8_WIDTH];
 static uint16_t chip8_gfx_last[CHIP8_HEIGHT][CHIP8_WIDTH];
+static uint16_t screen_gfx[SCREEN_HEIGHT][SCREEN_WIDTH];
+
 
 uint16_t sys_paddata;
 unsigned long sys_msec_timer;
 
 static unsigned long sys_usec_timer;
 static unsigned short rcnt1_last;
-
 static DISPENV dispenv;
+
+
+static void scale_chip8_gfx(void)
+{
+	float xratio = ((float)CHIP8_WIDTH)/SCREEN_WIDTH;
+	float yratio = ((float)CHIP8_HEIGHT)/SCREEN_HEIGHT;
+	float px, py;
+	int i, j;
+
+	for (i = 0; i < SCREEN_HEIGHT; ++i) {
+		for (j = 0; j < SCREEN_WIDTH; ++j) {
+			py = floor(i * yratio);
+			px = floor(j * xratio);
+			screen_gfx[i][j] =
+			  sys_chip8_gfx[(int)py][(int)px] ? 0xFFFF : 0x0000;
+		}
+	}
+}
+
 
 void init_systems(void)
 {
@@ -57,13 +79,6 @@ void update_display(void)
 	static int fps = 0;
 	static unsigned long msec_last = 0;
 
-	RECT chip8_area = {
-		.x = (SCREEN_WIDTH / 2) - CHIP8_WIDTH,
-		.y = (SCREEN_HEIGHT / 2) - CHIP8_HEIGHT,
-		.w = CHIP8_WIDTH,
-		.h = CHIP8_HEIGHT
-	};
-
 	++fps;
 
 	if ((sys_msec_timer - msec_last) >= 1000) {
@@ -71,11 +86,14 @@ void update_display(void)
 		FntPrint("FPS: %d", fps);
 		FntFlush(-1);
 		fps = 0;
+		DrawSync(0);
 	}
 
 	if (fps == 0 || memcmp(sys_chip8_gfx, chip8_gfx_last, sizeof chip8_gfx_last) != 0) {
-		LoadImage(&chip8_area, (void*)&sys_chip8_gfx);
+		scale_chip8_gfx();
+		LoadImage2(&dispenv.disp, (void*)&screen_gfx);
 		memcpy(chip8_gfx_last, sys_chip8_gfx, sizeof chip8_gfx_last);
+		DrawSync(0);
 	}
 
 }
